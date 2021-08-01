d2c8b9d5ed89b8603323e042a962c90db3f62831
KValexander <ewoase@gmail.com>
2021-08-01 17:04:35 +0300

    client upd

--- server/controllers/ModerationController.php
+++ server/controllers/ModerationController.php
@@ -4,8 +4,9 @@ include "config/validator.php";
 include "config/auth.php";
 
 class ModerationController {
+	// Get users
 	public function getUsers(){
-		$users = DB::table("users")->get();
+		$users = DB::table("users")->where("delete_marker", "=", "0")->get();
 		return response(200, $users);
 	}
 }
