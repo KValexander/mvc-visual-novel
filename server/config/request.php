224d6da36429f0deabdeb757611ec6871f9b9b5b
KValexander <ewoase@gmail.com>
2021-08-22 10:06:20 +0300

    request upd

--- server/config/request.php
+++ server/config/request.php
@@ -6,12 +6,18 @@ class Request {
 
 	// Returns all data
 	public static function all() {
-		$array = array_merge($_REQUEST, $_FILES);
+		if($_SERVER["CONTENT_TYPE"] ==  'application/json') {
+			$data = file_get_contents('php://input');
+			$array = json_decode($data, true);
+		} else $array = array_merge($_REQUEST, $_FILES);
 		return $array;
 	}
 	// Returns data by key
 	public static function input($key) {
-		$array = array_merge($_REQUEST, $_FILES);
+		if($_SERVER["CONTENT_TYPE"] ==  'application/json') {
+			$data = file_get_contents('php://input');
+			$array = json_decode($data, true);
+		} else $array = array_merge($_REQUEST, $_FILES);
 		return $array[$key];
 	}
 
